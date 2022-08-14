d80b5458f7dd0e0bdb7d9031696069f77f767881
moahmed amine mezghich <git@mezghich.com>
2022-08-14 14:39:55 +0100

    pharma-front

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'frontPharma'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontPharma');
+    expect(app.title).toEqual('Flash Pharma v1.0');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontPharma app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Flash Pharma v1.0');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontPharma';
+  title = 'Flash Pharma v1.0';
 }
